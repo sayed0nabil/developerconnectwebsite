62cbb280b24b5ad8d19434f1da62d09ff2757854
Seko Nabil <sayed0nabil@gmail.com>
2019-04-10 20:35:20 +0200

    Deploying

--- app.js
+++ app.js
@@ -11,8 +11,7 @@ const users   = require('./routes/api/users'),
       posts   = require('./routes/api/posts');
 // Default Settings
 const app = express(),
-      port  =  5000,
-      // process.env.PORT ||
+      port  =  process.env.PORT || 4000,
     //   mongodbURI = 'mongodb+srv://seko:seko@mernstackfronttoback-qiptm.mongodb.net/test?retryWrites=true';
          mongodbURI = require('./config/keys').MONGO_URI;
 app.use(bodyParser.json());
