188a622c538bd567309dafdcf79cea18fe0de160
Seko Nabil <sayed0nabil@gmail.com>
2019-04-10 16:45:09 +0200

    Deploying

--- config/keys_pro.js
+++ config/keys_pro.js
@@ -1,11 +1,6 @@
 
 
-// module.exports = {
-//     MONGO_URI: process.env.MONGO_URI,
-//     secretOrKey: process.env.SECRET_OR_KEY
-// }
 module.exports = {
-    localMongoURI: 'mongodb://localhost:27017/merntfronttoback',
-    MONGO_URI: 'mongodb+srv://seko:seko@mernstackfronttoback-qiptm.mongodb.net/mernstackfronttoback?retryWrites=true',
-    secretOrKey: 'somesecret'
+    MONGO_URI: process.env.MONGO_URI,
+    secretOrKey: process.env.SECRET_OR_KEY
 }
