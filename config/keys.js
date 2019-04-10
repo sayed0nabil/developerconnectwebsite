0b602fc5490c2006ccb7bfe3f09bec90c248f031
Seko Nabil <sayed0nabil@gmail.com>
2019-04-10 17:07:13 +0200

    Deploying

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'producation'){
     module.exports = require('./keys_pro');
 }else{
-    // module.exports = require('./keys_dev');
+    module.exports = require('./keys_pro');
 }
