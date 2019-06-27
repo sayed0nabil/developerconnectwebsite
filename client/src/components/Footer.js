46ba60fab7c639c90faa139a5e2cad097784bff5
Seko Nabil <sayed0nabil@gmail.com>
2019-06-27 16:15:20 +0200

    Styling

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div className='bg-dark text-white text-center footer'>
+    <div style={{
+      padding: '15px 0'
+    }}
+    className='bg-dark text-white text-center footer'>
         copyright &copy; {new Date().getFullYear()} Sayed Nabil
     </div>
   )
